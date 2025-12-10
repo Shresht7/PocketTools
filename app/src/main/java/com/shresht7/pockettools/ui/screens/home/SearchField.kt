938faebdccd21f183e7649afea20af8f518e1ae7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-12-10 12:48:43 +0530

    chore(home): Remove unused Preview import
    Removes an unused import for `androidx.compose.ui.tooling.preview.Preview` from the `SearchField.kt` file.

--- app/src/main/java/com/shresht7/pockettools/ui/screens/home/SearchField.kt
+++ app/src/main/java/com/shresht7/pockettools/ui/screens/home/SearchField.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shresht7.pockettools.R
 
